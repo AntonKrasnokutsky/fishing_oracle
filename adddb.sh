f7815aaf707560e622da56cacb135f2b44300e82
Anton <anton.krasnokutsky@gmail.com>
2020-04-02 21:56:26 +0300

    start

---
+++ adddb.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#echo "Adding new MySQL database and user"
+
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
+
+echo "Имя базы: "
+read MAINDB
+
+# replace "-" with "_" for database username
+MAINDB=${MAINDB//[^a-zA-Z0-9]/_}
+
+echo "Имя пользователя: "
+read USERNAME
+
+echo "Пароль пользователя базы: "
+read -s PASSWDDB
+
+# If /root/.my.cnf exists then it won't ask for root password
+if [ -f /root/.my.cnf ]; then
+    mysql -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+    mysql -e "CREATE USER ${USERNAME}@localhost IDENTIFIED BY '${PASSWDDB}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${USERNAME}'@'localhost';"
+    mysql -e "FLUSH PRIVILEGES;"
+
+# If /root/.my.cnf doesn't exist then it'll ask for root password   
+else
+    ${SUDO} mysql -uroot -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+    ${SUDO} mysql -uroot -e "CREATE USER ${USERNAME}@localhost IDENTIFIED BY '${PASSWDDB}';"
+    ${SUDO} mysql -uroot -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${USERNAME}'@'localhost';"
+    ${SUDO} mysql -uroot -e "FLUSH PRIVILEGES;"
+fi
+
+echo ""
+echo "Создана база данных '${MAINDB}' и пользователь '${USERNAME}'."
+echo ""
--- fishing_oracle/fishing/admin.py
+++ fishing_oracle/fishing/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Fish
+from .models import Fish_Trophy
+from .models import Fishing_Result
 
-# Register your models here.
+admin.site.register(Fish)
+admin.site.register(Fish_Trophy)
+admin.site.register(Fishing_Result)
