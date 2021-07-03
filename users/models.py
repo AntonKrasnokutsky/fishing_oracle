c3f2d7cf9b1cb5eeef18d46192ee023377b7c660
Антон Краснокутский <59136983+AntonKrasnokutsky@users.noreply.github.com>
2021-07-03 09:05:27 +0300

    Модель пользователя

--- users/models.py
+++ users/models.py
@@ -14,6 +14,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
                               unique=True)
     nick = models.CharField(max_length=50,
                             unique=False,
+                            default='Рыболов'
                             verbose_name='Ник')
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
