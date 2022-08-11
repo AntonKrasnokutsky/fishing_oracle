4e18071c1cc93e9e54410608f686e04fa0d6fe99
Антон Краснокутский <59136983+AntonKrasnokutsky@users.noreply.github.com>
2022-08-11 18:07:19 +0300

    admin off

--- fishing_oracle/settings.py
+++ fishing_oracle/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.admin',
+    # 'django.contrib.admin',
 ]
 
 MIDDLEWARE = [
--- fishing_oracle/urls.py
+++ fishing_oracle/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
-from django.contrib import admin
+# from django.contrib import admin
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('fishing.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
